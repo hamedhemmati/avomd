111fa1a1f6068fff052b4640dc2f2af0d25d15fc
Hamed Hemmati <hamed@hemmati.us>
2021-03-19 17:59:56 -0700

    sample mockend json

--- src/components/patients/Patients.js
+++ src/components/patients/Patients.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import PatientListComponent from "./PatientList";
 import SearchComponent from "./search";
-
+const axios = require("axios");
 export default function PatientsComponent({}) {
   useEffect(() => {}, []);
   return (
